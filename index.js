53824852d728d4af7f7cb4a05c5ea4d6e35d6743
Sawaira Noor <126554695+SawairaNoor@users.noreply.github.com>
2024-06-23 21:55:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,547 @@
+// ASSIGNMENT NO 01:
+
+ // Q1) WRITE A SCRIPT TO GREET YOUR WEBSITE VISITOR JS ALERT BOX(WELCOME TPO OUR WEBSITE)?
+// let greet = alert("Welcome to our website");
+
+// Q2)WRITE A SCRIPT TO DISPLAY MESSAGE ON YOUR WEB PAGE (ERROR ! PLEASE  ENTER A VALID PASSWORD)?
+ //let Password = prompt("Enter the Password!");
+ //alert("ERROR ! PLEASE  ENTER A VALID PASSWORD");
+
+//Q3) WRITE A SCRIPT TO DISPLAY MESSAGE ON YOUR WEB PAGE(HINT: LINE BREAK)?
+
+//Q4) WRITE A SCRIPT TO DISPLAY MESSAGE IN SEQUENCE(3 ALERTS)?
+ //alert("hello");
+// alert("how are you");
+// alert("have a good day");
+
+// // Q5) MAKE USE OF alert in  NEW AND EXISTING HTML AND CSS FILE?
+ //alert("Add your cart");
+
+// // Q6) 
+
+
+// // Q7) DECLARE A VARIABLE NAME CALLED USERNAME?
+ //let username;
+
+// //Q8) DECLARE A VARIABLE NAME CALLED MYNAME ANS ASSIGN TO IT A STRING THAT REPRESENTOING YOUR FULL NAME?
+ //let myName=("sawaira noor");
+
+//Q9) WRITE SCRIPT TO A) DEC;LARE A JS VARIABLE, TITLED MESSAGE B) ASSIGN HELLO WORLD TO VARIABLE MESSGAE C)  DISPLAY 
+// THE MESSGAE IN ALERT BOX?
+ //let js=("HelloWorld");
+//alert(js);
+
+//Q10) WRITE A SCRIPT TO SAVE STUDENTS BIO DATA IN JS VARIABLES TO SHOW THE DATA IN ALERT BOXES?
+ //let std_age = 19;
+ //let std_name=("sawaira noor");
+ //let std_field=("Information Technology and LLB");
+
+ //let std_biodata = "Student Age: " + std_age + "\n" +
+                // "Student Name: " + std_name + "\n" +
+              //"Student Field: " + std_field;
+
+
+// alert(std_age);
+/// alert(std_name);
+/// alert(std_field);
+// alert(std_biodata);
+
+//Q11) DECLARE A VARIABLE CALLED EMAIL AND ASSIGN TO IT A STRING THAT REPRESENTS YOUR EMAIL ADDRESS
+//(EG: EXAMPLE@EXAMPLE.COM) SHOW THE BLOW MENTIONED  MESSAGE IN AN ALERT BOX. (hINT: USING STRING CANCATENATE)?  
+//let email=("sawairanoor97@gmail.com");
+//let message= "Your email address is  " + email;
+ //alert(message);
+
+// Q12) WRITE A SCRIPT TO DISPLAY THIS IN BROWSER THRPOUGH JS?
+//let email=("sawairanoor97@gmail.com");
+ //let message= "Your email address is  " + email;
+  //alert(message);
+ //document.write("<p>" + message + "<p>");
+
+// Q13) STORE FOLOWING STRING IN A VARIABLE ANS SHOW IN ALERT AND BROWSER THROUGH JS?
+//let email=("sawairanoor97@gmail.com");
+ //alert(email);
+ //document.write("<p>" + email + "<p>" );
+
+// Q14) DECLARE A VRAIAB;E CALLED AGE AND ASSIGN VALUE TO YOUR AGE .SHOW YOUR AGE IN ALERT BOX?
+// let age= 19;
+// alert(age);
+
+// 15) DECLARE AN INITIALIZE A VARIABLE TO KEEP TRACK OF HOW MANY TIMES A VISITOR HAS VISITED
+//A WEB PAGE. FOR EG: (YOU HAVE VISITED THIS SITE N TIMES??)
+//  let visitCount=0;
+// visitCount++;
+// document.write("<p>" + visitCount + "times" + "<p>");
+
+// Q16) declare a varible called birth year and asisgn it to your birth year .show the following
+//message in yout browser?
+
+// let birth_year=2004;
+// document.write("<p>" + birth_year + "<p>");
+
+//Q17) A VISITOR VISITS AN ONLINE CLOTHING STORE WRITE A STORE IN VARIABLES THE FOOLOOWING INFORMATION:
+// A) VISITORS NAME B) PRODUCT NAME C) QUANTITY I.E HOW MANY PRODUCTS WANTS TO ORDER SHOW THE FOLLOWING 
+//MESSAGE IN ITHE BROWSER,"JOHN DOE ORDERED 5 T-SHIRTS ON XYZ CLOTHING STORE"?
+// let store=("www,.xyzclothing.com");
+// let visitor_name=("John Doe") ;
+// let product_name=("T-shirts");
+// let quantity=5;
+// let message= visitor_name + "  Ordered  "+ quantity + product_name + "  ON XYZ CLOTHING STORE";
+// document.write("<p>" + message + "<p>");
+
+//Q18) DECLARE 3 VARUABLES IN ONE STATEMENT?
+// let visitor_name=("John Doe") , product_name=("T-shirts") , quantity=5;
+
+// Q19) DECLARE 5 LEGAL AND ILLEGAL VARIABLES NAME?
+// let variablesName="and"; 
+// document.write(
+//   "<p>ILLEGAL VARIABLES</p>" +
+//   "<ul>" +
+//     "<li>let full name</li>" +
+//     "<li>let @name</li>" +
+//     "<li>let 1name</li>" +
+//     "<li>let full-name</li>" +
+//     "<li>let let;</li>" +
+//   "</ul>" +
+//   variablesName + "<br>" +
+//   "<p>LEGAL VARIABLES</p>" +
+//   "<ul>" +
+//     "<li>let _fullName</li>" +
+//     "<li>let fullName1</li>" +
+//     "<li>let full_name</li>" +
+//     "<li>let $fullName</li>" +
+//     "<li>let fullName</li>" +
+//   "</ul>"
+// );
+
+
+
+// Q20) DISPLAY THIS IN YOUR BROWSEER A) A HEADING STTAING "RULES FOR NAMING JS VARIABLES" (B)VARIABLES 
+//NAME CAN ONLY CONTAIN .......,....... , ....... AND ........  . FOR EXAMPLE $MY_1stVariable? (c) VARIABLES 
+//MUST BEGIN WITH A ......... ,. ...... OR ........  FOR EXAMPLE $NAME ,_NAME OR NAME? (d) VARIABLES NAMES ARE
+//CASE ......... (e) VARIABLES NAMES SHOULD NOT BE JS .......?
+
+// let message="RULES FOR NAMING JS VARIABLES       ";
+//         document.write("<p>" + message + "<p>");
+
+// let Variables_name= "Variable names can only contain letters, numbers, underscores (_), and dollar signs ($).<br>" +
+//                              "1. Dollar signs ($)<br>" +
+//                              "2. Underscores (_)<br>" +
+//                              "3. No spaces";
+//              document.write("<p>" + Variables_name + "<p>");
+
+//         let variablesBegin= "Variables Must Begin With a dollar, underscore OR letters " 
+//         document.write("<P>" + variablesBegin + "<P>");
+
+//         let variables=" \n  Variables names are case sensitive."
+//         document.write("<P>" + variables + "<P>");
+
+//         let variables_dont="Variable names should not be JS reserved keywords."
+//         document.write("<P>" + variables_dont + "<P>");
+
+
+// Q21) WRITE A PROGRAM THAT TAKE TWO NUMBERS AND ADD THEM IN A NEW VARIABLE SHOW THE RESULT IN BROWSER?
+
+// let num1= prompt("Enter the first number");
+// let num2= prompt("Enter the second numbers");
+
+// num1=parseFloat(num1);
+// num2=parseFloat(num2);
+
+// let add = num1 + num2;
+// document.write("<P>" + add + "<P>");
+
+//Q22) REPEAT TASK 21 FOR SUBTRACTION,MULTIPLICATION,DIVISION AND MODULUS?
+
+// subtraction:
+// let num1= prompt("Enter the first number");
+// let num2= prompt("Enter the second numbers");
+
+// num1=parseFloat(num1);
+// num2=parseFloat(num2);
+
+// let sub = num1 - num2;
+// document.write("<P>" + sub + "<P>");
+
+// multiplication:
+// let num1= prompt("Enter the first number");
+// let num2= prompt("Enter the second numbers");
+
+// num1=parseFloat(num1);
+// num2=parseFloat(num2);
+
+// let mult = num1 * num2;
+// document.write("<P>" + mult + "<P>");
+
+// Division:
+// let num1= prompt("Enter the first number");
+// let num2= prompt("Enter the second numbers");
+
+// num1=parseFloat(num1);
+// num2=parseFloat(num2);
+
+// let div = num1 / num2;
+// document.write("<P>" + div + "<P>");
+
+// Modulus:
+// let num1 = prompt("Enter the first number");
+// let num2 = prompt("Enter the second numbers");
+
+// num1=parseFloat(num1);
+// num2=parseFloat(num2);
+
+// let mod = num1 % num2;
+// document.write("<P>" + mod + "<P>");
+
+
+// Q23) DO THE FOLLOWING USING JS MATHEMATICAL EXPRESSIONS: (A) DECLARE A VARIABLE (B) SHOW THE VALUE OF 
+//VARIABLE IN YOUR BROWSER LIKE "VALUE AFTER VARIABKLE DECLERATION IS:" // (c) INITIALIZE THE VARIABLE WITH 
+//SOME NUMBER (d) SHOW THE VALUE OF VARIABLE IN BROWSER LIKE "INITIAL VALUE:5" (e) INCREMENT THE VARIABLE 
+//(f)  SHOW THE VALUE OF VARIABLE IN BROWSER LIKE "VALUE AFTER INCREMENT IS: 6" (g) ADD 7 TO THE VARIABLE 
+//(H) SHOW THE VALUE OF VARIABLE IN BROWSER LIKE "VALUE AFTER ADDITION  IS: 13" (i) DECREMENT THE VARIABLE 
+//(j)  SHOW THE VALUE OF VARIABLE IN BROWSER LIKE "VALUE AFTER DECREMENT IS: 12" (k) SHOW THE REMAINDER 
+//AFTER DIVIDING THE VARIABLE'S VALUE BY 3 (l) OUTPUT: THE REAMINDER IS ZERO?? 
+
+// PART A:
+// let num;
+// // // Part B:
+// document.write("<P> VALUE AFTER VARIABLE DECLERATION IS   :   " +  num + "<P>");
+// // // // part c:
+// num =5;
+// // // // part d:
+// document.write("<P> Initial Value:" + num + "<P>");
+// // // // part e:
+//  num++;
+// // // // part f:
+// document.write("<P> VALUE AFTER INCREMENT IS:" + num + "<P>");
+// // // // part g:
+// num += 7;
+// // // // part h:
+//  document.write("<P> VALUE AFTER ADDITION  IS: " + num + "<P>");
+// // // // part i:
+//  num--;
+// // // // part j:
+//  document.write("<P> VALUE AFTER DECREMENT IS:" + num + "<P>");
+// // // // part k:
+//  num%=3;
+// // // // part l:
+//  document.write("<P> Output the remainder is      " + num + "  <P>");
+
+
+// Q24)COST OF ONE MOVIE TICKET IS 600PKR . WRITE A SCRIPT TO STORE A TICKET PRICE IN A VARIABLE AND 
+//CALCULATE THE COST OF BUYING 5 TICKETS TO A MOVIE???
+// let ticketPrice= 600;
+// let totalCost= ticketPrice*= 5;
+// document.write("<P> The cost of buying 5 tickets to a movie is:  " + totalCost + "  PKR  <P>");
+
+
+// Q25) WRITE A SCRIPT TO DISPLAY A MULTIPLICATION TABLE OF ANY NUMBER IN YOUR BROWSER???
+// let num= prompt("Enter the number  : ");
+// let table = "";
+
+//         // Loop to generate the multiplication table
+//         for(let i = 1; i <= 12; i++) {
+//             let product = num * i;
+//             table += num + " x " + i + " = " + product + "<br>";
+//         }
+
+//         // Display the multiplication table in the browser
+//         document.write("<p>" + table + "</p>");
+// 
+
+                                              //ASSIGNMENT 02:
+
+
+// Q1) WRITE A PROGRAM TO TAKE A NUMBER IN A VARIABLE , DO THE REQUIRED AIRTHMETIC TO DISPLAY THE FOLLOWING RESULT 
+// IN YOUR BROWSER??
+// let a = Number(prompt("Enter number! ")); 
+// document.write("<p> RESULT : <br> The value of a is: " + a + "</p>");
+
+// ++a;
+// document.write("<p>The value of ++a is: " + a + "<br> Now, the value of a is: " + a + "</p>");
+
+// a++;
+// document.write("<p>The value of a++ is: " + a + "<br> Now, the value of a is: " + a + "</p>");
+
+// --a;
+// document.write("<p>The value of --a is: " + a + "<br> Now, the value of a is: " + a + "</p>");
+
+// a--;
+// document.write("<p>The value of a-- is: " + a + "<br> Now, the value of a is: " + a + "</p>");
+
+
+// Q2) WHAT WILL BE THE OUTPUT OF VARIABLES A,B AND RESULT AFTER EXECUTION OF THE FOLLOWING SCRIPT : 
+//VAR A=2,B=1; VAR RESULT = --A - --B + ++B + B--??
+// EXPLAIN THE OUTPUT AT EACH STAGE ?? 
+// A) --a
+// b) --a - --b
+// c) --a - --b + ++b
+// d) --a - --b + ++b + b--??
+
+// // // // // a) --a
+// let a = 2;
+// let b = 1;
+// --a;
+//  a = 2;
+// b = 1;
+// document.write("<p>" + (--a - --b + ++b + b--) + "</p>");
+
+// document.write("<P> The value of a and b is " + a + "  " + b + "<P>");
+// document.write("<P>" + a + "<P>");
+
+// // // // // // b) --a - --b
+// a = 2;
+// b = 1;
+// --a - --b;
+// document.write("<P>" + (--a - --b) + "<P>");
+
+// // // // // // c) --a - --b + ++b
+// a = 2;
+// b = 1;
+// --a - --b + ++b;
+// document.write("<P>" + (--a - --b + ++b)+ "<P>");
+
+// // // d) --a - --b + ++b + b--
+
+// Q3) WRITE A PROGRAM THAT TAKES INPUT A NAME FROM USER AND GREET THE USER??
+// let input= prompt("Enter your name :  ");
+// document.write("<P> Welcome  " + input +  " To this page !  <P>");
+
+
+// Q4) WRITE A PROGRAM THAT TAKE INPUT A NUMBER FROM USER AND DISPLAY ITS MULTIPLICATION TABLE ON YOUR BROWSER. 
+// IF USER DOES NOT ENTER A NEW NUMBER , MULTIPLICATION TABLE OF 5 SHOULD BE DISPLAYED BY DEFAULT??
+
+// let num = prompt("Enter number");
+
+// if (!num || isNaN(num)) {
+//     num = 5; // Default to 5 if no valid number is entered
+// } else {
+//     num = Number(num); // Convert the input to a number
+// }
+
+// let table = "  ";
+// for(let i=1; i<=12 ; i++){
+//    let product = num*i;
+//    table += num + "  *  " + i + "  =  "  + product + " <br> ";
+// }
+// document.write("<P>" + table + "<P>");
+
+
+//Q5) TAKE
+// A) TAKE THREE SUBJECTS NAME FROM USER AND STORE THEM IN 3 DIFFERENT VARIABLES.
+// B) TOTAL MARKS OF EACH SUBJECT IS 100,STORE IT IN ANOTHER VARIABLE.
+// C) TAKE OBTAINED MARKS FOR FIRST SUBJECT FROM USER AND STORED IT IN ANOTHER VARIABLE.
+// D) TAKE OBTAINED MARKS FOR REMAINING TWO SUBJECTS FROM USER AND STORE THEM IN VARIABLES.
+// E) NOW CALCULATE TOTAL MARKS AND PERCENTAGE AND SHOW THE RESULT IN BROWSER LIKE THIS???
+
+// let sub1 = prompt("Enter first subject name 1: ");
+// let sub2 = prompt("Enter first subject name 2: ");
+// let sub3 = prompt("Enter first subject name 3: ");
+
+
+// console.log("TOTAL MARKS AND PERCENTAGE") ;
+// console.log("....................... \n ");
+
+// let total_sub_each_marks = 100;
+// let total_sub_marks = 300;
+
+// let obt_marks_sub1 = 95;
+// let obt_marks_sub2 = 94;
+// let obt_marks_sub3 = 78;
+
+// let percentage1 = (obt_marks_sub1 / total_sub_each_marks) * 100;
+// let percentage2 = (obt_marks_sub2 / total_sub_each_marks) * 100;
+// let percentage3 = (obt_marks_sub3 / total_sub_each_marks) * 100;
+
+// console.log(sub1 + "  " + total_sub_each_marks + "   " + obt_marks_sub1 + "  " + percentage1 + "  % " ); 
+// console.log(sub2 + "  " + total_sub_each_marks + "   " + obt_marks_sub2 + "  " + percentage2 + "  % " ); 
+// console.log(sub3 + "  " + total_sub_each_marks + "   " + obt_marks_sub3 + "  " + percentage3 + "  % " ); 
+
+
+// total_obt_marks = obt_marks_sub1 + obt_marks_sub2 + obt_marks_sub3;
+// let percentage = (total_obt_marks / total_sub_marks * 100);
+
+// console.log("Total percentage of 3 subjects is " + percentage) ;
+
+                                     
+                                      // ASSIGNMENT NO 03:
+
+// Q1) WRITE A PROGRAM TO TAKE "CITY" NAME AS INPUT FROM USER. IF USER ENTERS "KARACHI" WELCOME THE USER LIKE THIS: 
+//"WELCOME TO CITY OF LIGHTS"???
+
+// let city=prompt("Enter the city name:  ");
+// document.write("<P>" + city + "<P>");
+// if( city === "karachi" ){
+//     document.write("<P> WELCOME TO CITY OF LIGHTS <P>" );
+// } else {
+//     document.write("<P> It is not city of lights  <P>" );
+// }
+
+// Q2) WRITWE A PROGRAM TO TAKE "GENDER" AS INPUT FROM USER. IF THE USER IS MALE GIVE THE MESSAGE: 
+//GOOD MORNING SIR . IF THE USER IS FEMALE, GIVE THE MESSAGE: GOOD MORNING MAAM ?? 
+
+// let gender= prompt("Enter your gender");
+// document.write("<P>" + gender + "<P>");
+
+// if(gender=== "male"){
+// document.write("<P> Good Morning Sir");
+// } 
+// else if(gender === "female"){
+//     document.write("<P> Good Morning Maam");
+// } else{
+//     document.write("<P> not prefer to tell");
+// }
+
+// Q3) WRITE A PROGRAM TO TAKE INPUT REMAINING FUEL IN CAR (IN LITRES) FROM USER.IF THECURRENT FUEL 
+//IS LESS THAN 0.25 LITRES, SHOW THE MESSSAGE "PLEASE REFILL THE FUEL IN YOUR CAR"???/
+
+// let fuel = parseFloat(prompt("Please enter the remaining fuel in your car (in liters):"));
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car.");
+// } else {
+//     alert("You have enough fuel.");
+// }
+
+
+//Q4) RUN THIS SCRIPT AND CHECK WHETHER ALERT MESSAGES WOULD BE DISPLAYED OR NOT .
+// RECORD THE OUITPUTS.
+
+// PART A:
+// var a=4;
+// if(++a == 5){
+//     alert("given condition for variable a is true");
+// }
+
+
+// // PART b:
+// var b=82;
+// if(b++ == 83){
+//     alert("given condition for variable b is true");
+// }
+
+// // partc:
+// var c=12;
+// if(c++ === 13){
+//     alert("condition 1 is true");
+// } if(c===13){
+//     alert("condition 2 is true");
+// } if( ++c < 14){
+//     alert("condition 3 is true");
+// } if (c=== 14){
+//     alert("condition 4 is true");
+// }
+
+// // PART D:
+// let materialCost=20000;
+// let laborCost=2000;
+// let totalCost=materialCost+laborCost;
+// if(totalCost=materialCost+laborCost){
+//     alert("the cost equals");
+// }
+
+// // PART E:
+// if(true){
+//     alert("true");
+// }if(false){
+//     alert("false");
+// }
+
+// // part f:
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+
+// Q5) GUESS GAME: STORE A SECRET NUMBER RANGING  FROM (1_10) IN A VARIABLE. PROPMPT USER TO GUESS THE SECRET NUMBER.
+//A) IF USER GUESSES THE SAME NUMBER , SHOE "BINGO , CORRECT ANSWER" . (B) IF THE GUESSED NUMBER +1 IS THE SECRET
+//NUMBER SHOW THE MESSAGE "CLOSE ENPOUGH TO THE SECRET NUMBER"???
+
+// let secret_number=6;
+// let guess_number = parseInt(prompt("gues sthe secret number"));
+// if(guess_number === secret_number){
+//     console.log("BINGO , CORRECT ANSWER");
+// } 
+// else if(guess_number + 1 === secret_number){
+// console.log("CLOSE ENPOUGH TO THE SECRET NUMBER");
+// } else{
+//     console.log("Better luck next Time");
+// }
+
+//Q6) WRITE A PROGRAM TO CHECK WHETHER THE GIVEN NUMBER IS DIDVISIBLE BY 3 .SHOW THE MESSGAE TO THE USER 
+//IF THE NUBER IS DIVISIBLE BY 3.
+
+// let given_num= prompt("enter the number");
+// if(given_num % 3== 0){
+// console.log("num is divisible by 3");
+// } else {
+//     console.log("num is not divisible by 3");
+// }
+
+
+// Q7) WRITE A PROGRAM TO CHECK WHETHER THE GIVEN INPUT IS EVENOR ODD NUMBER???
+// let given_num = parseInt(prompt("Enter your input number"));
+// if(given_num % 2 ==0){
+//     console.log("Numer is even");
+// } else {
+//     console.log("number is odd");
+// }
+
+
+// Q8) WRITE A PROGRAM TAHT TAKE TEMPEERATURE AS INPUT AND SHOWS A MESSAGE BASED ON A FOLLOWING CRITERIA???
+// A) T>40 THEN "IT IS TOO HOT OUTSIDE"
+// B) T>30 THEN "TODAY WHETHER IS NORMAL"
+// C) T>20 THEN "TODAYS WHETHER IS COOL"
+// D) T>10 THEN "OMG! TODAYS WHETHER IS SO COOL"???
+
+// let temperature= prompt("what is the temperature today ? ");
+// if(temperature>40){
+//     console.log("IT IS TOO HOT OUTSIDE");
+// }
+// else if(temperature>30){
+//     console.log("TODAY WHETHER IS NORMAl");
+// }
+// else if(temperature>20){
+//     console.log("TODAYS WHETHER IS COOl");
+// }
+// else if(temperature>10){
+//     console.log("OMG! TODAYS WHETHER IS SO COOL");
+// } else{
+//     console.log("Its really freezing outside");
+// }
+
+
+//Q9) WRITE APROGRAM TO CREATE A CALCULATOR FOR +,-,*,/ AND % USINF IF STATEMENTS . TAKE THE FOLLOWEEING INPUT 
+//A) FIRST NUMBER (b) SECOND NUM,BER  (c) OPERATIONS (+,-,%,*,/) COMPUTE AND SHOW THE RESULT TO USER??? 
+
+// let num1=parseInt(prompt("Enter first number"));
+// let num2=parseInt(prompt("Enter second number"));
+// let operations=prompt("Enter any opeartor (+ , - , * , / , %");
+// let result;
+
+// if(operations == "+" ){
+//          result =  num1 + num2;
+// } 
+// else if(operations === "-"){
+//     result= num1-num2;
+// }
+// else if(operations == "*"){
+//     result = num1 * num2;
+// }
+// else if(operations == "/"){
+//     if(num2 !== 0){
+//         result=num1/num2;
+//     } else{
+//         result= "error! division is not allowed by zero 0";
+//     }
+// }
+// else if(operations == "%"){
+//     if(num2 !== 0){
+//         result = num1 % num2
+//     } else{
+//         result= "error! division is not allowed by zero 0";
+//     }
+// } else {
+//     result = "Error: Invalid operation.";
+// }
+
+// console.log("The result is: " + result);
